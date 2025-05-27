a3553bb30a8aaa68cb4e69c112a93c584fc71f99
Whitek323 <whitek8327@gmail.com>
2025-05-27 08:18:58 +0700

    test md

---
+++ CPP/factorial.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int number_1;
+    cin >> number_1;
+    int temp = number_1;
+    
+    if(number_1 < 0){
+      cout << "น้อยกว่า 0";
+    }
+    if((number_1+1) % 2 ==0){
+      cout << "เลขคี่";
+      if(number_1 % 2 != 0){
+          cout << "Is Decimal ";
+      }
+    }
+    
+    for(int i = 1;i < number_1;i++){
+      int round = number_1 - i;
+      
+      temp = temp * round;
+    }
+    
+    cout << temp;
+    return 0;
+}
