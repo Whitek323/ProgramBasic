de0aa7e086b23e72ac106e455aa17e542c8cc278
Whitek323 <whitek8327@gmail.com>
2025-07-31 10:44:11 +0700

    fib 1/2

--- CPP/train/20.cpp
+++ CPP/train/20.cpp
@@ -1,3 +1,39 @@
 // 20. จงเขียนโปรแกรมรับรายการคะแนนนักเรียน แล้วคำนวณค่าเฉลี่ย และแสดงข้อความว่า
 // “สอบผ่าน” ถ้าเกณฑ์ ≥ 50, “สอบไม่ผ่าน” ถ้า < 50
 // พร้อมนับจำนวนผู้สอบผ่านและแสดงผลท้ายโปรแกรม
+
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main(){
+    int std_number;
+    cout << "ใส่จำนวนนักเรียน : ";
+    cin >> std_number;
+    list<int> std_score = {};
+
+    int i = 1;
+    while(i <= std_number){
+        int x;
+        cout << "ใส่คะแนน : ";
+        cin >> x;
+        std_score.push_back(x);
+        i++;
+    }
+    float sum_std_score = 0;
+    for(int item:std_score){
+        sum_std_score += item;
+        if(item < 50){
+            cout << item << " : สอบไม่ผ่าน" << endl;
+        }else if(item >= 50){
+            cout << item << " : สอบผ่าน" << endl;
+        }
+    }
+    cout << "จำนวนผู้เข้าสอบ : " << std_score.size()<< endl;
+    cout << "ค่าเฉลี่ย : " << sum_std_score/std_score.size()<< endl;
+ 
+
+    return 0;
+}
--- CPP/train/8.cpp
+++ CPP/train/8.cpp
@@ -1,2 +1,25 @@
 // จงเขียนฟังก์ชัน fib(n) เพื่อคืนค่าลำดับ Fibonacci ที่ตำแหน่ง n (กำหนด fib(0)=0,
 // fib(1)=1) และให้โปรแกรมรับค่า n แล้วแสดง fib(n)
+//ลำดับ 0 1 2 3 4 5 6 7
+//fib 0 1 1 2 3 5 8 13
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+void fib(int n){
+    int x = 0;
+    for(int i=1;i<=n;i++){
+        cout << i << endl;
+    }
+    cout << x;
+
+}
+int main(){
+    int n;
+    cout << "ใส่ค่า fibo : ";
+    cin >> n;
+    // cout << "Fibonacci คือ " << fib(n) << endl;
+    fib(n);
+    return 0;
+}
